a8770d4fbbd588f9bc37d477c2b86d5059da61cc
johnmcauley <john@alkimii.com>
2021-03-15 13:33:45 +0100

    Add all project files

---
+++ src/components/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <GridLayout columns="*" rows="*, 50">
+      <ListView row="0" ref="itemsListView" for="(item, index) in items" separatorColor="transparent">
+        <v-template >
+          <ListItem :item="index"/>
+        </v-template>
+      </ListView>
+      <GridLayout row="1" columns="*, *">
+        <Button col="0" text="Go Back" @tap="navigateBack()"/>
+        <Button col="1" text="Next Page" @tap="nextPage()"/>
+      </GridLayout>
+    </GridLayout>
+  </Page>
+</template>
+
+<script >
+  // Components
+  import ListItem from './ListItem'
+  import Main from './App'
+  import {GC} from 'utils/utils'
+
+  export default {
+    components: {
+      ListItem
+    },
+    data() {
+      return {
+        msg: 'Hello World!',
+        items: new Array(1045),
+        indexkk: 0
+      }
+    },
+    
+    methods: {
+      nextPage() {
+        this.$navigateTo(Main, {
+          clearHistory: true
+        })
+      },
+      navigateBack() {
+        this.$navigateBack()
+        GC()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <GridLayout columns="50, *" rows="100, 400" height="300">
+    <Image row="0" col="0" :src="imageUrl()" stretch="aspectFit"/>
+    <Label row="0" col="1" :text="randomText(5)"/> 
+    <!--<VideoPlayer
+      row="1"
+      src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+      :autoplay="false"
+      :fill="VideoFill.aspect"
+      :controls="true"/>-->
+    <GridLayout row="1" col="1" rows="50,50,50" columns="*, *, *, *, *, *, *, *, *">
+      <Image row="0" col="0" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="1" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="2" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="3" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="4" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="5" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="6" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="7" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="0" col="8" :src="imageUrl()" stretch="aspectFit"/> 
+
+      <Image row="1" col="0" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="1" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="2" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="3" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="4" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="5" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="6" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="7" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="1" col="8" :src="imageUrl()" stretch="aspectFit"/> 
+
+      <Image row="2" col="0" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="1" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="2" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="3" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="4" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="5" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="6" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="7" :src="imageUrl()" stretch="aspectFit"/> 
+      <Image row="2" col="8" :src="imageUrl()" stretch="aspectFit"/> 
+    </GridLayout>
+    
+  </GridLayout>
+</template>
+
+<script>
+
+import { VideoFill } from '@nstudio/nativescript-exoplayer'
+
+export default {
+  props: {
+    item: {
+      required: false,
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      VideoFill
+    }
+  },
+  methods: {
+    randomText(length) {
+      var result           = '';
+      var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var charactersLength = characters.length;
+      for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    },
+    imageUrl() {
+      if(parseInt(this.item % 2) === 0)
+        return 'https://picsum.photos/200'
+      return 'https://picsum.photos/200'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
